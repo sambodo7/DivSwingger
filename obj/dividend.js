1352b504c8049b3ad9665fc6e8c2b306bec750db
Dell <ssteven352@gmail.com>
2020-09-17 12:49:00 +0100

    stocks

---
+++ obj/dividend.js
@@ -0,0 +1,11 @@
+class Dividend {
+	constructor(symbol, companyName, exDate, payDate, divRatio, divPayment, stockCost) {
+		this.symbol = symbol;
+		this.companyName = companyName;
+		this.exDate = exDate;
+		this.payDate = payDate;
+		this.divRatio = divRatio;
+		this.divPayment = divPayment;
+		this.stockCost = stockCost;
+	}
+}
